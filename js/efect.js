1ba2fcd7e02cdefb9fec1c6d3e8bd8a6bd619652
Jose <96209618+ch0c0tr0l@users.noreply.github.com>
2025-04-07 18:34:52 -0600

    actualizacion 070425
    se ajustan imagenes y se agrega nueva fila para siguiente seccion

---
+++ js/efect.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY;
+    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercent = (scrollTop / docHeight) * 100;
+
+    const line = document.querySelector('.line');
+    line.style.width = scrollPercent + '%';
+});
