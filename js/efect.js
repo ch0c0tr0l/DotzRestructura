3e08bed19255211173887fc93bd921a059a91b99
Jose <96209618+ch0c0tr0l@users.noreply.github.com>
2025-04-13 01:44:57 -0600

    cambios3

--- js/efect.js
+++ js/efect.js
@@ -6,3 +6,47 @@ window.addEventListener('scroll', () => {
     const line = document.querySelector('.line');
     line.style.width = scrollPercent + '%';
 });
+
+window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY;
+    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercent = (scrollTop / docHeight) * 100;
+    const line2 = document.querySelector('.line2');
+    line2.style.width = scrollPercent + '%';
+    line2.style.transform = 'translateX(calc(100% - ' + scrollPercent + '%))';
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollElements = document.querySelectorAll('.scroll-reveal');
+
+    const elementInView = (el, dividend = 1) => {
+        const elementTop = el.getBoundingClientRect().top;
+        return (
+            elementTop <=
+            (window.innerHeight || document.documentElement.clientHeight) / dividend
+        );
+    };
+
+    const displayScrollElement = (element) => {
+        element.classList.add('scrolled');
+    };
+
+    const hideScrollElement = (element) => {
+        element.classList.remove('scrolled');
+    };
+
+    const handleScrollAnimation = () => {
+        scrollElements.forEach((el) => {
+            if (elementInView(el, 1.25)) {
+                displayScrollElement(el);
+            } else {
+                hideScrollElement(el);
+            }
+        });
+    };
+
+    window.addEventListener('scroll', () => {
+        handleScrollAnimation();
+    });
+});
